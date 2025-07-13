46d3ec75831eb073ffa9eb0392d5634845925d4e
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2025-07-13 16:19:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Sidebar from "./components/Sidebar";
 import FinanceDashboard from "./pages/FinanceDashboard";
 import DoctorDashboard from "./pages/DoctorDashboard";
 import EngagementDashboard from "./pages/EngagementDashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <div className="flex h-screen bg-blue-400">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto p-6">
-          <Routes>
-            <Route path="/" element={<FinanceDashboard />} />
-            <Route path="/finance" element={<FinanceDashboard />} />
-            <Route path="/doctor" element={<DoctorDashboard />} />
-            <Route path="/engagement" element={<EngagementDashboard />} />
-          </Routes>
-        </main>
+      <div className="flex-1">
+        <Navbar />
+        <div className="flex h-screen bg-blue-400">
+          <Sidebar /> 
+          <main className="p-6"><Routes>
+              <Route path="/" element={<FinanceDashboard />} />
+              <Route path="/finance" element={<FinanceDashboard />} />
+              <Route path="/doctor" element={<DoctorDashboard />} />
+              <Route path="/engagement" element={<EngagementDashboard />} />
+              </Routes></main>
+        </div>
       </div>
     </Router>
   );
